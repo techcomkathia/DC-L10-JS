542bb3e3b60c5bb5b5c9218a02167a9bed9d6c27
techcomkathia <techcomk@gmail.com>
2026-01-21 20:25:39 -0300

    passos iniciais

---
+++ NODE/aula49/Projeto/comandosTerminal.js
@@ -0,0 +1,20 @@
+// # Inicializa o projeto
+// npm init -y && \
+// # Instala dependências
+// npm install express sequelize mysql2 dotenv bcrypt jsonwebtoken && \
+// # Instala dependências de desenvolvimento
+// npm install --save-dev nodemon && \
+// # Cria estrutura de diretórios
+// mkdir src
+// mkdir src\config
+// mkdir src\database
+// mkdir src\models
+// mkdir src\routes
+// mkdir src\controllers
+// mkdir src\services
+// mkdir src\middlewares
+// mkdir src\utils
+
+// type nul > src\app.js
+// type nul > src\server.js
+// type nul > .env
